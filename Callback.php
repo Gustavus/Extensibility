88b073fa3f818ed9731b732f3d1ab77015137f8a
Joe Lencioni <jlencion@gustavus.edu>
2012-03-20 15:37:21 -0500

    Added isFunction() and isMethod()

--- Callback.php
+++ Callback.php
@@ -56,9 +56,9 @@ class Callback
   private function getReflection()
   {
     if ($this->reflection === null) {
-      if (is_array($this->function)) {
+      if ($this->isMethod()) {
         $this->reflection = new \ReflectionMethod($this->function[0], $this->function[1]);
-      } else if (is_string($this->function)) {
+      } else if ($this->isFunction()) {
         $this->reflection = new \ReflectionFunction($this->function);
       }
     }
@@ -87,12 +87,32 @@ class Callback
   }
 
   /**
+   * Determines if this callback is a method on an object.
+   *
+   * @return boolean
+   */
+  private function isMethod()
+  {
+    return is_array($this->function);
+  }
+
+  /**
+   * Determines if this callback is a standard function (not an object's method).
+   *
+   * @return boolean
+   */
+  private function isFunction()
+  {
+    return is_string($this->function);
+  }
+
+  /**
    * @param array $arguments
    * @return mixed
    */
   public function execute(array $arguments = array())
   {
-    if (is_array($this->function)) {
+    if ($this->isMethod()) {
       return $this->getReflection()->invokeArgs($this->function[0], $arguments);
     } else {
       return $this->getReflection()->invokeArgs($arguments);
